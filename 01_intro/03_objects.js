6bd0bde5853e820ed02eecf4267889ab3ffeaef7
shubham kumari <kumarishubham273@gmail.com>
2024-09-21 08:11:33 +0530

    type aliases

---
+++ 01_intro/03_objects.js
@@ -0,0 +1,14 @@
+"use strict";
+// const user = {
+//     name: "shubham",
+//     email: "ex@gmail.com",
+//     isActive: false
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+var myUser = {
+    _id: "12",
+    name: "sh",
+    email: "s@s.com",
+    isActive: false
+};
+myUser.email = "s@gmail.com";
