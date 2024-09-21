21a889ebf4789f6d60d16e26ce92a3fc1073d65f
shubham kumari <kumarishubham273@gmail.com>
2024-09-21 08:25:46 +0530

    raray

---
+++ 01_intro/04_array.ts
@@ -0,0 +1,17 @@
+const superHeros: string[] = []
+// const heroPower: number[] = []
+const heroPower: Array<number> = []
+superHeros.push("spiderman")
+heroPower.push(2)
+
+type User = {
+    name: string,
+    isActive: boolean
+}
+const allUsers : User[] = []
+allUsers.push({name: "s", isActive: false})
+
+const mlModels: number[][] = [
+    [255, 255, 255],
+    []
+]
